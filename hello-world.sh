2388ec2b8bbdcec73d3e5f3f26744e6b21c7f410
Junwen Zheng <junwen.zheng@cern.ch>
2024-05-08 19:13:37 -0400

    Made a small change to hello-world.sh

--- hello-world.sh
+++ hello-world.sh
@@ -2,7 +2,7 @@
 
 
 echo -e "Running ${0} ...\n================"
-echo -e "Hello world! My name is ${1} ${2}, but most people call me ${3}."
+echo -e "Hello world! My name is ${1} ${2}, but most people call me ${3}. Just call me whatever you like."
 
 # If the script exits with something other than 0 the GitHub Action will fail.
 exit 0
